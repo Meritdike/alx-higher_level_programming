a91fda41a0da09d8d330348aeb05e3f9fee01b1e
Merit Dike <dike.merit@yahoo.com>
2022-12-07 06:34:59 -0800

    ch: task 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -7,6 +7,7 @@ void print_python_bytes(PyObject *p);
  * print_python_list - Prints basic info about Python lists.
  * @p: A PyObject list object.
  */
+
 void print_python_list(PyObject *p)
 {
 	int size, alloc, i;
